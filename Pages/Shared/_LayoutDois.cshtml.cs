7129331819c2170482ff1b516702281ae0a9cf08
Moises.alves <Moises.alves@atp.com.br>
2023-07-26 17:15:47 -0300

    MultiplosLayouts

---
+++ Pages/Shared/_LayoutDois.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyRazorApp.Pages.Shared
+{
+    public class _LayoutDoisModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/_ViewImports.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyRazorApp.Pages
+{
+    public class _ViewImportsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
