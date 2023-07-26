71f57ab753eb8666877d8e97bf8d92e1fb5f5aea
Moises.alves <Moises.alves@atp.com.br>
2023-07-26 17:48:15 -0300

    Parametros e Rotas

--- Pages/Categories.cshtml.cs
+++ Pages/Categories.cshtml.cs
@@ -6,12 +6,21 @@ namespace MyRazorApp.Pages
     public class CategoriesModel : PageModel
     {
         public List<Category> CategoryList { get; set; } = new();
-        public void OnGet()
+        public void OnGet(
+            int skip=0, 
+            int take=25)
         {
-            for (var i = 0; i <= 100; i++)
+            var temp = new List<Category>();
+
+            for (var i = 0; i <= 1787; i++)
             {
-                CategoryList.Add(new Category(i, $"Categoria {i}", i * 18.95M));
+                temp.Add(new Category(i, $"Categoria {i}", i * 18.95M));
             }
+
+            CategoryList = temp
+                .Skip(skip)
+                .Take(take)
+                .ToList();
         }
     }
 }
